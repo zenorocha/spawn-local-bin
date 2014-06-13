429d147620c781f24f193e66c3f45649c20d4e56
Zeno Rocha <zeno.rocha@liferay.com>
2014-06-13 18:03:26 -0300

    Fix process platform checker

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var path  = require('path');
 var spawn = require('child_process').spawn;
 
+var isWindows = process.platform.match(/^win/);
+
 module.exports = function(cmd, args, cwd) {
     this.args = args || [];
     this.cmd  = cmd || '';
     this.cwd  = cwd || process.cwd();
 
     // Append the .cmd extension to the command call on Windows
-    if (cmd && process.platform.match('/^win/')) {
+    if (cmd && isWindows) {
         this.cmd = this.cmd + '.cmd';
     }
 
@@ -21,6 +23,6 @@ module.exports.path = function(dir) {
     this.dir = dir || process.cwd();
 
     // Add the node_modules/.bin directory to the PATH
-    var PATH_SEPARATOR = process.platform.match(/^win/) ? ';' : ':';
+    var PATH_SEPARATOR = isWindows ? ';' : ':';
     process.env.PATH = path.join(dir, 'node_modules', '.bin') + PATH_SEPARATOR + process.env.PATH;
 };
